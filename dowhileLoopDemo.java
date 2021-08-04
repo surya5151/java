ed74fe0192bf5423cf81d5d8ba29e55ba5274baa
surya5151 <surya2021.xworkz@gmail.com>
2021-08-04 23:03:05 +0530

    do-while, breack, continue program

---
+++ dowhileLoopDemo.java
@@ -0,0 +1,13 @@
+class dowhileLoopDemo{
+public static void main(String[] args){
+System.out.println("program start");
+int count=10;
+
+do{
+System.out.println(count);
+count++;
+}
+while(count<=12);
+System.out.println("program end");
+}
+}
---
+++ whileLoopDemo.java
@@ -0,0 +1,12 @@
+class whileLoopDemo{
+public static void main(String[] args){
+System.out.println("program start");
+int count=5;
+while(count<=12){
+System.out.println(count);
+count++;
+}
+System.out.println("program end");
+}
+
+}
