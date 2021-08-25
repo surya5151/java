677797cb75e36b7d0d86af9a3a05a44b639185fe
surya5151 <surya2021.xworkz@gmail.com>
2021-08-25 12:02:09 +0530

    withInClass

---
+++ welcome/ConstructionChaining/com/xworks/java/welcome/Mobile.java
@@ -0,0 +1,38 @@
+package com.xworks.java.welcome;
+
+public class Mobile {
+	String brand;
+	String color;
+	double price;
+	
+	Mobile(){
+		this("one Plus","black");
+		this.brand=brand;
+		this.color=color;
+		System.out.println("Defult construction");
+	}
+	
+	Mobile(String brand){
+		this("one Plus","Black",20000.00);
+		this.brand=brand;
+		this.color=color;
+		this.price=price;
+		System.out.println("Single construction");
+	}
+
+	Mobile(String brand, String color){
+		//this(brand,color,20000.00);
+		this.brand=brand;
+		this.color=brand;
+		this.price=price;
+		System.out.println("Two construction");
+	}
+	
+	Mobile (String brand, String color, double price){
+		this.brand=brand;
+		this.color=color;
+		this.price=price;
+		System.out.println("Three construction");
+		
+	}
+}
---
+++ welcome/ConstructionChaining/com/xworks/java/welcome/MobileTest.java
@@ -0,0 +1,13 @@
+package com.xworks.java.welcome;
+
+public class MobileTest {
+
+	public static void main (String[] args) {
+		
+		//Mobile mobile = new Mobile();
+		//System.out.println(mobile.color);
+		
+		Mobile sony =new Mobile("sony","gold");
+		//System.out.println(sony.brand);
+	}
+}
