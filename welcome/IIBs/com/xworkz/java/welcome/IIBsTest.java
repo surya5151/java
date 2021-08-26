3373cdaa195f79cf4d4894f2b603fde818e157e2
surya5151 <surya2021.xworkz@gmail.com>
2021-08-26 12:28:25 +0530

    IIBS

---
+++ welcome/IIBs/com/xworkz/java/welcome/IIBsTest.java
@@ -0,0 +1,13 @@
+package com.xworkz.java.welcome;
+
+public class IIBsTest {
+	public static void main(String[] arga) {
+		
+		Refrigerators lg = new Refrigerators();
+		System.out.println(lg.brand);
+		System.out.println(lg.capacity);
+		System.out.println(lg.color);
+		System.out.println(lg.powerConsumption);
+		
+	}
+}
---
+++ welcome/IIBs/com/xworkz/java/welcome/Refrigerators.java
@@ -0,0 +1,29 @@
+package com.xworkz.java.welcome;
+
+public class Refrigerators {	
+		String brand;
+		String capacity;
+		String color;
+		String powerConsumption;
+		
+		Refrigerators(){
+			System.out.println("defult..!");
+		}
+		
+		{
+			System.out.println("IIBs1....");
+			brand="LG";
+			capacity="100ltrs";
+			color="Gray";
+			powerConsumption="230v";
+		}
+		
+		{
+			System.out.println("IIBs2...");
+			brand="Samsung";
+			capacity="50ltrs";
+			color="Black";
+			powerConsumption="230v";
+					
+		}
+}
