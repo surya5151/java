80a10aa8349f853b418835a0b567ed2c954872ab
surya5151 <surya2021.xworkz@gmail.com>
2021-08-31 12:20:36 +0530

    Object_HashCode

---
+++ welcome/Object/com/xworkz/java/Object_Clash/hashcode/HashCodeTest.java
@@ -0,0 +1,16 @@
+package com.xworkz.java.Object_Clash.hashcode;
+
+public class HashCodeTest{
+public static void main(String[] args) {
+		
+		HashCodeTest surya = new HashCodeTest();
+		System.out.println(surya.hashCode());
+		
+		HashCodeTest hashcodetest1 = new HashCodeTest();		
+		System.out.println(hashcodetest1);
+		System.out.println(hashcodetest1.toString());
+		
+	
+	}
+
+}
