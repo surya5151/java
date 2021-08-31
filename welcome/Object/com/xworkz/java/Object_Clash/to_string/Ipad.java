edb59d73a5e0b094bcced6d2c96916e63ca562ba
surya5151 <surya2021.xworkz@gmail.com>
2021-08-31 12:21:45 +0530

    Object_ToSting_Method

---
+++ welcome/Object/com/xworkz/java/Object_Clash/to_string/Ipad.java
@@ -0,0 +1,23 @@
+package com.xworkz.java.Object_Clash.to_string;
+
+public class Ipad {
+	String brand;
+	String ramsize;
+	String cameraQuality;
+	double price;
+	
+	Ipad(String brand, String ramsize, String cameraQuality, double price){
+		this.brand=brand;
+		this.ramsize=ramsize;
+		this.cameraQuality=cameraQuality;
+		this.price=price;
+		
+	}
+	
+	public String toString() {
+		return this.brand=brand +"\n"+ this.ramsize+"\n"+ 
+				this.cameraQuality+"\n"+ this.price;
+	   
+	}
+
+}
---
+++ welcome/Object/com/xworkz/java/Object_Clash/to_string/ToStringTest.java
@@ -0,0 +1,11 @@
+package com.xworkz.java.Object_Clash.to_string;
+
+public class ToStringTest {
+	public static void main(String[] args) {
+		Ipad ipad = new Ipad("Apple","5GB", "32MP",40000);
+		
+		System.out.println(ipad.toString());
+		
+	}
+
+}
