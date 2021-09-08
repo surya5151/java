a7514d9d6dd9ba0215565ddae741dc6fa29bb3f5
surya5151 <surya2021.xworkz@gmail.com>
2021-09-08 23:26:56 +0530

    Assignment-array

---
+++ welcome/Arrays/com/xworkz/java/arrays_Assignment/MaxNumber.java
@@ -0,0 +1,30 @@
+package com.xworkz.java.arrays_Assignment;
+
+public class MaxNumber {
+
+	public static void main(String[] args) {
+		
+		int[][] arr= new int[3][3];
+		
+		int count=10;
+		int max= arr[0][0];
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				arr[i][j]=count;
+				count=count+10;
+				if(arr[i][j]>max)
+					max=arr[i][j];				
+			}			
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println("--------------------------------------");
+		System.out.println("Maximum number in the given Array is "+max);
+
+	}
+}
---
+++ welcome/Arrays/com/xworkz/java/arrays_Assignment/Sum.java
@@ -0,0 +1,26 @@
+package com.xworkz.java.arrays_Assignment;
+
+public class Sum {
+	public static void main(String[] args) {
+		int[][] arr= new int[2][4];
+				int count=1;
+				int sum= arr[0][0];
+				
+				for(int i=0; i<arr.length; i++) {
+					for(int j=0; j<arr[i].length; j++) {
+						arr[i][j]=count;
+						count=count+1;	
+						sum=arr[i][j]+sum;
+					}					
+				}
+	
+				for(int i=0; i<arr.length; i++) {
+					for(int j=0; j<arr[i].length; j++) {
+					System.out.print(arr[i][j] +" ");				
+					}	
+					System.out.println();
+				}
+				System.out.println("-------------------------------");
+				System.out.println("Sum of all the element in array is:"+sum);
+	}
+}
